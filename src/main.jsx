be64c0ec170fec8e89658ad474612b9a9855a3c0
Justin Wachtel <jnp3ju@virginia.edu>
2025-06-06 20:34:24 -0400

    fix: wrap app in ChakraProvider for theme and color support

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import FMVCalculator from "./pages/FMVCalculator";
+import { ChakraProvider } from "@chakra-ui/react";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <Routes>
-      <Route path="/*" element={<App />} />
-      <Route path="/calculator" element={<FMVCalculator />} />
-    </Routes>
-  </Router>
+  <ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+        <Route path="/calculator" element={<FMVCalculator />} />
+      </Routes>
+    </Router>
+  </ChakraProvider>
 );
