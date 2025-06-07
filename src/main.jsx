8806e9dcb065a837d5c8d110e93b19147a00cde0
Justin Wachtel <jnp3ju@virginia.edu>
2025-06-07 16:39:42 -0400

    main update

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import FMVCalculator from "./pages/FMVCalculator";
 import { ChakraProvider } from "@chakra-ui/react";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
     <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-        <Route path="/calculator" element={<FMVCalculator />} />
-      </Routes>
+      <App />
     </Router>
   </ChakraProvider>
 );
