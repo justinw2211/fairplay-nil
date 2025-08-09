000b78f531c4892f0cd63a631f3598fb353969f9
Justin Wachtel <jnp3ju@virginia.edu>
2025-08-09 11:57:16 -0400

    test(blog): fix duplicate import blocks in Blog components

--- frontend/src/pages/Blog/BlogList.jsx
+++ frontend/src/pages/Blog/BlogList.jsx
@@ -37,43 +37,4 @@ const BlogList = () => {
 
 export default BlogList;
 
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { Container, VStack, Card, CardBody, Heading, Text, Button, HStack, Box } from '@chakra-ui/react';
-import { getAllPosts } from '../../data/blogPosts';
-
-const formatDate = (iso) => new Date(iso).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
-
-const BlogList = () => {
-  const posts = getAllPosts();
-
-  return (
-    <Container maxW="4xl" py={12}>
-      <VStack align="stretch" spacing={6}>
-        <Heading size="xl" color="brand.textPrimary">Blog</Heading>
-        {posts.map((p) => (
-          <Card key={p.slug} bg="white" borderColor="brand.accentSecondary">
-            <CardBody>
-              <VStack align="start" spacing={3}>
-                <Heading size="md" color="brand.textPrimary">{p.title}</Heading>
-                <HStack color="brand.textSecondary" fontSize="sm">
-                  <Text>{formatDate(p.date)}</Text>
-                  <Box as="span">•</Box>
-                  <Text>{p.author}</Text>
-                </HStack>
-                <Text color="brand.textSecondary">{p.excerpt}</Text>
-                <Button as={RouterLink} to={`/blog/${p.slug}`} alignSelf="flex-start" bg="brand.accentPrimary" color="white" _hover={{ opacity: 0.9 }}>
-                  Read more
-                </Button>
-              </VStack>
-            </CardBody>
-          </Card>
-        ))}
-      </VStack>
-    </Container>
-  );
-};
-
-export default BlogList;
-
 
